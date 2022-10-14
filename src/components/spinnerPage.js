cb54b72589fd9146034a569bf8b6392bb97eeba3
Rémi Castaing <remi.castaing@gmail.com>
2022-10-14 13:01:54 +0200

    fix spinner

--- src/components/spinnerPage.js
+++ src/components/spinnerPage.js
@@ -1,8 +1,16 @@
 import Spinner from 'react-bootstrap/Spinner';
 
 
-export default function SpinnerPage (){
+export default function SpinnerPage() {
 
 
-    return (<Spinner animation="border" variant="primary" />)
+    return (
+        <div className="d-flex flex-column h-100 center">
+            <div className="p-2 flex-fill"></div>
+            <div class="d-flex justify-content-center">
+                <Spinner animation="border" variant="primary" />
+            </div>
+            <div className="p-2 flex-fill"></div>
+        </div>
+    )
 }
